f737a1f9b0505481fa660df0ae2f8ad04d8082e7
Alexei <alexeiiv164@gmail.com>
2023-05-07 22:28:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.reviews__content').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        infinite: true,
+    });
+});
